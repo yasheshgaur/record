09d83fdedafb830f0c5799c4f81f5ef4ce2fd957
Yashesh Gaur <yasheshgaur@gmail.com>
2016-03-13 18:47:10 -0400

    corrected upload

--- php/upload.php
+++ php/upload.php
@@ -1,10 +1,9 @@
 <?php
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL | E_STRICT);
+	$data = $_REQUEST['audio'];
+	$ourFileName = "uploads/voiceFile.wav";
+	$fh = fopen($ourFileName, 'w') or die("can't open file");
+	fwrite($fh, $data);
+	fclose($fh);
 ?>
-
-$data = $_REQUEST['audio'];
-$ourFileName = "uploads/voiceFile.wav";
-$fh = fopen($ourFileName, 'w') or die("can't open file");
-fwrite($fh, $data);
-fclose($fh);
